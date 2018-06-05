6e8f25b0bdae294c3555e0b09b389c0c9242509a
Priyasharma16 <39939945+Priyasharma16@users.noreply.github.com>
2018-06-05 22:40:39 +0530

    sum of two numbers

---
+++ Fourthdemo.java
@@ -0,0 +1,11 @@
+class Fourthdemo
+ {
+public static void main(String[] args)
+ {
+ String s1= args[0];
+ String s2= args[1];
+ int num1= Integer.parseInt(s1);
+ int num2= Integer.parseInt(s2);
+   System.out.println("sum ="+(num1+num2));
+ }
+}
