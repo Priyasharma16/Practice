7e5c4a1ab1464ec06b8b9bcd19f2c081cf23dcdc
Priyasharma16 <39939945+Priyasharma16@users.noreply.github.com>
2018-06-05 23:44:06 +0530

    Either number is even or not

---
+++ SixthDemo.java
@@ -0,0 +1,16 @@
+class SixthDemo
+ {
+public static void main(String[] args)
+ {
+String s1=args[0];
+ int num= Integer.parseInt(s1);
+if(num%2==0)
+{
+System.out.println("Given number is even");
+ }
+else
+{
+System.out.println("Given number is not even number ");
+ }
+}
+ }
